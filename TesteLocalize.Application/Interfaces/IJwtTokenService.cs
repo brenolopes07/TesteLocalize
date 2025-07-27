ed087d55ddc76c38f3976066fd763bebacec67f6
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-27 16:08:04 -0300

    feat: implement interface JwtService and implementation Service JWT

---
+++ TesteLocalize.Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,10 @@
+﻿
+using TesteLocalize.Domain.Entities;
+
+namespace TesteLocalize.Application.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ TesteLocalize.Infra/Services/JwtTokenService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TesteLocalize.Application.Interfaces;
+using TesteLocalize.Domain.Entities;
+
+namespace TesteLocalize.Infra.Services
+{
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly string _secret;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly int _expirationMinutes;
+    
+
+    public JwtTokenService(IConfiguration configuration)
+        {
+            _secret = configuration["Jwt:Secret"];
+            _issuer = configuration["Jwt:Issuer"];
+            _audience = configuration["Jwt:Audience"];
+            _expirationMinutes = int.Parse(configuration["Jwt:ExpirationMinutes"]);
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+           {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("name", user.Name),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _issuer,
+                _audience,
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(_expirationMinutes),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
