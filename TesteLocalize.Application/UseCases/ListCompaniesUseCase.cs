5675ed0d596cd3fdad81c4225b6b1f21e9baa470
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-27 16:11:17 -0300

    feat; create useCase list companies by user

---
+++ TesteLocalize.Application/UseCases/ListCompaniesUseCase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteLocalize.Domain.Entities;
+using TesteLocalize.Domain.Repository;
+
+namespace TesteLocalize.Application.UseCases
+{
+    public class ListCompaniesUseCase
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public ListCompaniesUseCase(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+
+        public async Task<IEnumerable<Company>> ExecuteAsync(Guid userId)
+        {
+            var companies =  await _companyRepository.GetByUserIdAsync(userId);
+            if(companies == null)
+                throw new Exception("No companies found for the given user ID.");
+
+            return companies;
+        }
+    }
+}
