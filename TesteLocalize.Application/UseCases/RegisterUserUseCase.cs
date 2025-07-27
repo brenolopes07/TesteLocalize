539fcf0774c61809f1f3f2fdce6730ac4b5e4cff
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-27 16:11:59 -0300

    feat: create registerUser

---
+++ TesteLocalize.Application/UseCases/RegisterUserUseCase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteLocalize.Application.Interfaces;
+using TesteLocalize.Domain.Entities;
+using TesteLocalize.Domain.Repositories;
+
+namespace TesteLocalize.Application.UseCases
+{
+    public class RegisterUserUseCase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+
+
+        public RegisterUserUseCase(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<User> ExecuteAsync(string name, string email, string password)
+        {
+            var existingUser = await _userRepository.GetByEmailAsync(email);
+            if(existingUser != null)
+            {
+                throw new InvalidOperationException("User with this email already exists.");
+            }
+
+            var passwordHash = _passwordHasher.HashPassword(password);
+
+            var user = new User(name, email, passwordHash);
+
+            await _userRepository.AddAsync(user);
+
+            return user;
+        }
+    }
+}
