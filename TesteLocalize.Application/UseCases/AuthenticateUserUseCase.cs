4a7c79b9027b7a19b4384db818625d7a58b58308
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-27 16:10:51 -0300

    Feat: create useCase Authenticate

---
+++ TesteLocalize.Application/UseCases/AuthenticateUserUseCase.cs
@@ -0,0 +1,35 @@
+﻿
+using TesteLocalize.Application.Interfaces;
+using TesteLocalize.Domain.Repositories;
+
+namespace TesteLocalize.Application.UseCases
+{
+    public class AuthenticateUserUseCase
+    {
+        private readonly IUserRepository _userepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IJwtTokenService _jwtTokenService;
+
+        public AuthenticateUserUseCase(IUserRepository userepository, IPasswordHasher passwordHasher, IJwtTokenService jwtTokenService)
+        {
+            _userepository = userepository;
+            _passwordHasher = passwordHasher;
+            _jwtTokenService = jwtTokenService;
+        }
+
+        public async Task<string> ExecuteAsync(string email, string password)
+        {
+            var user = await _userepository.GetByEmailAsync(email);
+
+            if (user == null)
+                throw new Exception("Invalid email or password.");
+
+            if(!_passwordHasher.VerifyPassword(user.PasswordHash, password))
+            {
+                throw new Exception("Invlaid email or password.");
+            }
+
+            return _jwtTokenService.GenerateToken(user);
+        }
+    }
+}
