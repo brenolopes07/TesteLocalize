e234d7963f4c0394ff628cfafcfef7a46f4f25ba
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-28 21:48:39 -0300

    feat: update CompanyController List to pagination listcomanies GET

--- TesteLocalize.WebAPI/Controllers/CompanyController.cs
+++ TesteLocalize.WebAPI/Controllers/CompanyController.cs
@@ -31,27 +31,7 @@ namespace TesteLocalize.WebAPI.Controllers
                     return Unauthorized(new { error = "Invalid user ID in token." });
 
                 var company = await _registerCompanyUseCase.ExecuteAsync(userId, request.Cnpj);
-
-                var response = new CompanyResponse
-                {
-                    Id = company.Id,
-                    Name = company.Name,
-                    FantasyName = company.FantasyName,
-                    Cnpj = company.CNPJ,
-                    Situation = company.Situation,
-                    OpeningDate = company.OpeningDate,
-                    Type = company.Type,
-                    LegalNature = company.LegalNature,
-                    MainActivity = company.MainActivity,
-                    Street = company.Street,
-                    Number = company.Number,
-                    Complement = company.Complement,
-                    Neighborhood = company.Neighborhood,
-                    City = company.City,
-                    State = company.State,
-                    ZipCode = company.ZipCode
-                };
-
+                
                 return Created();
             }
             catch (Exception ex)
@@ -61,42 +41,17 @@ namespace TesteLocalize.WebAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> List()
+        public async Task<IActionResult> GetCompanies([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
         {
-            try
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? User.FindFirstValue(JwtRegisteredClaimNames.Sub);
+            if (!Guid.TryParse(userId, out var parsedUserId))
             {
-                var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? User.FindFirstValue(JwtRegisteredClaimNames.Sub);
-                if (!Guid.TryParse(userIdString, out var userId))
-                    return Unauthorized(new { error = "Invalid user ID in token." });
-
-                var companies = await _listCompaniesUseCase.ExecuteAsync(userId);
-
-                var response = companies.Select(c => new CompanyResponse
-                {
-                    Id = c.Id,
-                    Name = c.Name,
-                    FantasyName = c.FantasyName,
-                    Cnpj = c.CNPJ,
-                    Situation = c.Situation,
-                    OpeningDate = c.OpeningDate,
-                    Type = c.Type,
-                    LegalNature = c.LegalNature,
-                    MainActivity = c.MainActivity,
-                    Street = c.Street,
-                    Number = c.Number,
-                    Complement = c.Complement,
-                    Neighborhood = c.Neighborhood,
-                    City = c.City,
-                    State = c.State,
-                    ZipCode = c.ZipCode
-                });
-
-                return Ok(response);
+                return Unauthorized(new { error = "Invalid user ID in token." });
             }
-            catch (Exception ex)
-            {
-                return BadRequest(new { error = ex.Message });
-            }
+
+            var result = await _listCompaniesUseCase.ExecuteAsync(parsedUserId, page, pageSize);
+
+            return Ok(result);
         }
     }
 }
